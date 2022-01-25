d4ed71fb0b7c23fd9d5bfca2f1dcb66fa4c2bd89
VAUTIER Paul <vautier.paul14350@gmail.com>
2022-01-25 18:03:03 +0100

    templating homepage + q&a

--- AuroreApp/routes.py
+++ AuroreApp/routes.py
@@ -5,5 +5,16 @@ from AuroreApp import app
 
 @app.route("/", methods=["GET"])
 def home_page():
-    print("test")
     return render_template("index.html")
+
+@app.route("/faq", methods=["GET"])
+def faq():
+    return render_template("faq.html")
+
+@app.route("/pro", methods=["GET"])
+def pro():
+    return render_template("faq.html")
+
+@app.route("/particulier", methods=["GET"])
+def particulier():
+    return render_template("faq.html")
