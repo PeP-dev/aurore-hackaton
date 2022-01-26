32c68ed849672a2796ef0d04bac7e6c26f0911f9
Glock Matteo <mglock@outlook.fr>
2022-01-26 09:39:12 +0100

    fix issue not sending request

--- AuroreApp/models/sql_aurore.py
+++ AuroreApp/models/sql_aurore.py
@@ -25,9 +25,11 @@ class AuroreSQL(sql_request.Client):
     
     def check(self,table:str,kwargs:dict) :
         if table in AuroreSQL.TABLES :
+            if len(kwargs) != len(AuroreSQL.TABLES[table]) :
+                raise KeyError(f"Les données fournies ne correspondent pas aux attributs de la table {table}. Données : {kwargs} \n Données attendues : {AuroreSQL.TABLES[table]}")
             for kwarg in kwargs :
                 if not(kwarg in AuroreSQL.TABLES[table]) :
-                    raise KeyError(f"Les données fournies ne correspondent pas aux attributs de la table {table}. Données : {kwargs}")
+                    raise KeyError(f"Les données fournies ne correspondent pas aux attributs de la table {table}. Données : {kwargs} \n Données attendues : {AuroreSQL.TABLES[table]}")
         else :
             raise KeyError(f"La table {table} n'existe pas")
 
@@ -36,8 +38,14 @@ class AuroreSQL(sql_request.Client):
         liste = [(a,kwargs[a]) for a in kwargs]
         names = [a[0] for a in liste]
         values = [a[1] for a in liste]
-        self.fast_insert(table,names,values)
-        
+        sql_string = self.fast_insert(table,names,values)
+        self.process_send(sql_string)
+    
+    def delete(self,table:str,pkey_name,pkey):
+        sql_string = self.fast_delete_on_pk(table,pkey_name,pkey)
+        self.process_send(sql_string)
+
+    
 if __name__ == "__main__" :
     with AuroreSQL() as client :
         client.insert('CONDITIONS',id=1,libelle='oui')
